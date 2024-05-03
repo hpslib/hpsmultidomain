Fixed normal derivative operator to include edges/corners

domain_driver.py
@@ -244,18 +244,16 @@ class Domain_Driver:
         self.sparse_assembly = sparse_assembly
         self.solver_type     = solver_type
         ########## sparse assembly ##########
-        if self.d==2:
-            if (sparse_assembly == 'reduced_cpu'):
-                device = torch.device('cpu')
-                tic = time()
-                self.A,assembly_time_dict    = self.hps.sparse_mat(device,verbose)
-                toc_assembly_tot = time() - tic
-            elif (sparse_assembly == 'reduced_gpu'):
-                device = torch.device('cuda')
-                tic = time()
-                self.A,assembly_time_dict    = self.hps.sparse_mat(device,verbose)
-                toc_assembly_tot = time() - tic
+        if (sparse_assembly == 'reduced_cpu'):
+            device = torch.device('cpu')
+        elif (sparse_assembly == 'reduced_gpu'):
+            device = torch.device('cuda')
 
+        tic = time()
+        self.A,assembly_time_dict = self.hps.sparse_mat(device,verbose)
+        toc_assembly_tot = time() - tic
+        
+        if self.d==2:
             csr_stor  = self.A.data.nbytes
             csr_stor += self.A.indices.nbytes + self.A.indptr.nbytes
             csr_stor /= 1e9

hps_leaf_disc.py
@@ -17,7 +17,8 @@ JJ_2d    = namedtuple('JJ_2d', ['Jl','Jr','Jd','Ju','Jx','Jc','Jxreorder'])
 
 Pdo_3d = namedtuple('Pdo_3d', ['c11', 'c22', 'c33', 'c12', 'c13', 'c23', 'c1', 'c2', 'c3', 'c'])
 Ds_3d  = namedtuple('Ds_3d',  ['D11', 'D22', 'D33', 'D12', 'D13', 'D23', 'D1', 'D2', 'D3'])
-JJ_3d  = namedtuple('JJ_3d',  ['Jl', 'Jr', 'Jd', 'Ju', 'Jb', 'Jf', 'Jx', 'Jxreorder', 'Jxunique', 'Jc', 'Jtot'])
+JJ_3d  = namedtuple('JJ_3d',  ['Jl', 'Jr', 'Jd', 'Ju', 'Jb', 'Jf', 'Jx', 'Jlc', 'Jrc', 'Jdc',
+                               'Juc', 'Jbc', 'Jfc', 'Jxreorder', 'Jxunique', 'Jc', 'Jtot'])
 
 def cheb(p):
     """
@@ -305,6 +306,8 @@ def leaf_discretization_3d(a,p):
     zzG[4*p**2:,2] = zzCleanup[4*p**2:,2]
 
     JJ    = JJ_3d(Jl= Jl, Jr= Jr, Ju= Ju, Jd= Jd, Jb= Jb, Jf=Jf, Jx=Jx,
+                  Jlc=Jl_corner, Jrc=Jr_corner, Jdc=Jd_corner,
+                  Juc=Ju_corner, Jbc=Jb_corner, Jfc=Jf_corner,
                   Jxreorder=Jxreorder, Jxunique=Jxunique, Jc=Jc, Jtot=Jtot)
     return zz,Ds,JJ,hmin,zzG
 
@@ -316,13 +319,14 @@ def get_diff_ops(Ds,JJ,d):
         Nu = Ds.D2[JJ.Ju]
 
         Nx = np.concatenate((-Nl,+Nr,-Nd,+Nu))
-    else:
-        Nl = Ds.D1[JJ.Jl][:,JJ.Jtot]
-        Nr = Ds.D1[JJ.Jr][:,JJ.Jtot]
-        Nd = Ds.D2[JJ.Jd][:,JJ.Jtot]
-        Nu = Ds.D2[JJ.Ju][:,JJ.Jtot]
-        Nb = Ds.D3[JJ.Jb][:,JJ.Jtot]
-        Nf = Ds.D3[JJ.Jf][:,JJ.Jtot]
+    else: # Need to include corners here...
+        Jtot = np.hstack((JJ.Jc,JJ.Jxreorder))
+        Nl = Ds.D1[JJ.Jlc][:,Jtot]
+        Nr = Ds.D1[JJ.Jrc][:,Jtot]
+        Nd = Ds.D2[JJ.Jdc][:,Jtot]
+        Nu = Ds.D2[JJ.Juc][:,Jtot]
+        Nb = Ds.D3[JJ.Jbc][:,Jtot]
+        Nf = Ds.D3[JJ.Jfc][:,Jtot]
 
         Nx = np.concatenate((-Nl,+Nr,-Nd,+Nu,-Nb,+Nf))
     return Nx

hps_multidomain_disc.py
@@ -48,6 +48,8 @@ class HPS_Multidomain:
         self.p      = p
         self.a      = a
         self.d      = d
+
+        self.q = p
         
         n = (self.domain[:,1] - self.domain[:,0]) / (2*self.a)
         n = torch.round(n).int()
@@ -104,62 +106,79 @@ class HPS_Multidomain:
         - t_dict: Dictionary containing timing information for different parts of the matrix assembly process.
         """
         tic = time()
-        DtN_loc = self.get_DtNs(device,'build')
-        toc_DtN = time() - tic;
+        DtN_loc = self.get_DtNs(device,'build') # Tentatively think this is already good
+        toc_DtN = time() - tic
+        print(DtN_loc)
+        print(DtN_loc.shape)
         
-        size_face = self.p-2; size_ext = 4*size_face
-        n0,n1 = self.n
+        if self.d==2:
+            size_face = self.p-2; size_ext = 4*size_face
+            n0,n1 = self.n
+            nprod = n0*n1
+        else:
+            size_face = self.q**2; size_ext = 6*size_face
+            n0,n1,n2 = self.n
+            nprod=n0*n1*n2
         
-        tic = time()
-        Iext_box  = torch.arange(size_face).repeat(n0*n1,4).reshape(n0*n1,size_ext)
-        toc_alloc = time() - tic;
-        
-        tic = time()
-        # Go through range of n
-        for Npan_ind in range(n0):
-            # This increases in each level
-            npan_offset = (2*n1+1)*Npan_ind
+        if self.d==2:
+            tic = time()
+            Iext_box  = torch.arange(size_face).repeat(nprod,2*self.d).reshape(nprod,size_ext)
+            toc_alloc = time() - tic
             
-            for box_j in range(n1):
+            tic = time()
+            # Go through range of n
+            for Npan_ind in range(n0):
+                # This increases in each level
+                npan_offset = (2*n1+1)*Npan_ind
                 
-                box_ind = Npan_ind*n1 + box_j
-                
-                l_ind = npan_offset + box_j
-                r_ind = npan_offset + 2*n1 + 1 + box_j
-                d_ind = npan_offset + n1 + box_j
-                u_ind = npan_offset + n1 + box_j + 1
-                Iext_box[box_ind,:size_face]              += l_ind * size_face;
-                Iext_box[box_ind,size_face:2*size_face]   += r_ind * size_face;
-                Iext_box[box_ind,2*size_face:3*size_face] += d_ind * size_face;
-                Iext_box[box_ind,3*size_face:]            += u_ind * size_face
-        toc_index = time() - tic
+                for box_j in range(n1):
+                    
+                    box_ind = Npan_ind*n1 + box_j
+                    
+                    l_ind = npan_offset + box_j
+                    r_ind = npan_offset + 2*n1 + 1 + box_j
+                    d_ind = npan_offset + n1 + box_j
+                    u_ind = npan_offset + n1 + box_j + 1
+                    Iext_box[box_ind,:size_face]              += l_ind * size_face
+                    Iext_box[box_ind,size_face:2*size_face]   += r_ind * size_face
+                    Iext_box[box_ind,2*size_face:3*size_face] += d_ind * size_face
+                    Iext_box[box_ind,3*size_face:]            += u_ind * size_face
+            toc_index = time() - tic
+            
+            tic = time()
+            row_data,col_data = batched_meshgrid(n0*n1,size_ext,Iext_box,Iext_box)
+            
+            # LOOK HERE:
+            data = DtN_loc.flatten()
+            row_data = row_data.flatten()
+            col_data = col_data.flatten()
+            toc_flatten = time() - tic
+
+            toc_forloop = toc_index + toc_flatten + toc_alloc
+        else:
+            # For 3D, we're indexing box by box. Thus let's follow a similar approach here:
+            # We need an array
+            hmm = 1
         
-        tic = time()
-        row_data,col_data = batched_meshgrid(n0*n1,size_ext,Iext_box,Iext_box)
         
-        # LOOK HERE:
-        data = DtN_loc.flatten()
-        row_data = row_data.flatten()
-        col_data = col_data.flatten()
-        toc_flatten = time() - tic
-        
-        tic = time()
-        sp_mat = sp.coo_matrix(( np.array(data),\
-                                (np.array(row_data,dtype=int),np.array(col_data,dtype=int)))).tocsr()
-        sp_mat = sp_mat.tocsr()
-        toc_csr_scipy = time() - tic;
-        
-        toc_forloop = toc_index + toc_flatten + toc_alloc
-        
-        if (verbose):
-            print("\t--time to do for loop (alloc,index, flatten) (%5.2f,%5.2f,%5.2f)"\
-                  %(toc_alloc,toc_index,toc_flatten))
-            print("\t--time to assemble sparse HPS (DtN ops, for loop, csr_scipy) (%5.2f,%5.2f,%5.2f)"\
-                  %(toc_DtN,toc_forloop,toc_csr_scipy))
+        sp_mat = 0
+        if self.d==2:
+            tic = time()
+            sp_mat = sp.coo_matrix(( np.array(data),\
+                                    (np.array(row_data,dtype=int),np.array(col_data,dtype=int)))).tocsr()
+            sp_mat = sp_mat.tocsr()
+            toc_csr_scipy = time() - tic
+            
+            if (verbose) and (self.d==2):
+                print("\t--time to do for loop (alloc,index, flatten) (%5.2f,%5.2f,%5.2f)"\
+                    %(toc_alloc,toc_index,toc_flatten))
+                print("\t--time to assemble sparse HPS (DtN ops, for loop, csr_scipy) (%5.2f,%5.2f,%5.2f)"\
+                    %(toc_DtN,toc_forloop,toc_csr_scipy))
         t_dict = dict()
         t_dict['toc_DtN'] = toc_DtN
-        t_dict['toc_forloop'] = toc_forloop
-        t_dict['toc_sparse'] = toc_csr_scipy
+        if self.d==2:
+            t_dict['toc_forloop'] = toc_forloop
+            t_dict['toc_sparse'] = toc_csr_scipy
         return sp_mat,t_dict
     
     def get_grid(self):
@@ -368,8 +387,9 @@ class HPS_Multidomain:
         Jxreo = torch.tensor(self.H.JJ.Jxreorder).to(device)
         if d==3:
             Jxun  = torch.tensor(self.H.JJ.Jxunique).to(device)
-        Intmap= torch.tensor(self.H.Interp_mat).to(device)
-        Ds    = self.H.Ds.to(device)
+
+        Intmap = torch.tensor(self.H.Interp_mat).to(device)
+        Ds     = self.H.Ds.to(device)
         if (mode =='solve'):
             data = data.to(device)
 

hps_parallel_leaf_ops.py
@@ -128,7 +128,7 @@ def form_DtNs(p,d,xxloc,Nx,Jx,Jc,Jxreo,Jxun,Ds,Intmap,pdo,
 
     if (mode == 'build'):
         
-        if (pdo.c12 is None):
+        if (pdo.c12 is None) and (d==2):
             S_tmp   = -torch.linalg.solve(Acc,Aloc[:,Jc][...,Jx])
             Irep    = torch.eye(Jx.shape[0],device=device).unsqueeze(0).repeat(box_end-box_start,1,1)
             S_full  = torch.concat((S_tmp,Irep),dim=1)
@@ -137,10 +137,10 @@ def form_DtNs(p,d,xxloc,Nx,Jx,Jc,Jxreo,Jxun,Ds,Intmap,pdo,
             DtN     = Nx[...,Jtot].unsqueeze(0) @ S_full
         else:
             S_tmp   = -torch.linalg.solve(Acc,Aloc[:,Jc][...,Jxreo])
-            Irep    = torch.eye(Jx.shape[0],device=device).unsqueeze(0).repeat(box_end-box_start,1,1)
+            Irep    = torch.eye(Jxreo.shape[0],device=device).unsqueeze(0).repeat(box_end-box_start,1,1)
             S_full  = torch.concat((S_tmp @ Intmap.unsqueeze(0),Irep),dim=1)
             
-            Jtot    = torch.hstack((Jc,Jx))
+            Jtot    = torch.hstack((Jc,Jxreo)) # Might be Jx instead of Jxreo
             DtN     = Nx[...,Jtot].unsqueeze(0) @ S_full
             
             #S_tmp   = -torch.linalg.solve(Acc,Aloc[:,Jc][...,Jxreo])
@@ -203,6 +203,8 @@ def get_DtNs_helper(p,d,xxloc,Nx,Jx,Jc,Jxreo,Jxun,Ds,Intmap,pdo,\
                     box_start,box_end,chunk_init,device,mode,data,ff_body_func):
     nboxes = box_end - box_start
     size_face = (p-2)**(d-1)
+    if d==3:
+        size_face = p**2
     if (mode == 'build'):
         DtNs = torch.zeros(nboxes,2*d*size_face,2*d*size_face,device=device)
     elif (mode == 'solve'):