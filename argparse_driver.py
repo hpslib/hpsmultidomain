Fixed DtN build mode computation, now DtN unit test has decent results. Can get back to assembling A_CC correctly

argparse_driver.py
@@ -157,6 +157,14 @@ a = 1/(2*npan)
 dom = Domain_Driver(box_geom,op,\
                     kh,a,p=p,d=d,periodic_bc = args.periodic_bc)
 N = (p-2) * (p*dom.hps.n[0]*dom.hps.n[1] + dom.hps.n[0] + dom.hps.n[1])
+"""
+print("PDO:")
+from pprint import pprint
+pprint(vars(op))
+import inspect
+print(inspect.getsource(op.c))
+print(inspect.getsource(bfield))
+"""
 
 ################################# BUILD OPERATOR #########################
 # Build operator based on specified parameters and solver information
@@ -269,19 +277,23 @@ if (args.pickle is not None):
 
 ################################# EVALUATING SYSTEM COMPONENTS ###################################
 # Evaluate certain parts of the 3D problem 
-"""
-if (d==3):
+
+if (d==3 and 1==0):
     # First we generate the arrays of Chebyshev and Gaussian nodes:
     cheb_ext  = torch.from_numpy(dom.hps.H.zz.T[dom.hps.H.JJ.Jxreorder])
     gauss_ext = torch.from_numpy(dom.hps.H.zzG)
 
     uu_cheb  = uu_dir(cheb_ext)
     uu_gauss = uu_dir(gauss_ext)
-    uu_inter = torch.from_numpy(dom.hps.H.Interp_mat) @ uu_gauss
+    uu_interC = torch.from_numpy(dom.hps.H.Interp_mat) @ uu_gauss
+    uu_interG = torch.from_numpy(dom.hps.H.Interp_mat_reverse) @ uu_cheb
 
-    print("Relative error of interpolation is:")
-    print(torch.norm(uu_cheb - uu_inter) / torch.norm(uu_cheb))
+    print("Relative error of Gaussian-to-Chebyshev interpolation is:")
+    print(torch.norm(uu_cheb - uu_interC) / torch.norm(uu_cheb))
+    print("Relative error of Chebyshev-to-Gaussian interpolation is:")
+    print(torch.norm(uu_gauss - uu_interG) / torch.norm(uu_gauss))
 
+    """
     # Check if edges / corners are equal as expected:
     u, c = np.unique(dom.hps.H.JJ.Jxreorder, return_counts=True)
     dup = u[c > 1]
@@ -290,10 +302,11 @@ if (d==3):
         variance = torch.std(uu_inter[dom.hps.H.JJ.Jxreorder == elem])
         maxVar = np.max([maxVar, variance.item()])
     print("Largest variance between redundant values is " + str(maxVar))
+    """
 
 
 # Test the accuracy of I_copy1 and I_copy2:
-if d==3:
+if (d==3 and 1==0):
     zz_copy1 = dom.hps.gauss_xx[dom.hps.I_copy1,:]
     zz_copy2 = dom.hps.gauss_xx[dom.hps.I_copy2,:]
     print("Numerical error of copy1 vs copy2 is:")
@@ -302,18 +315,22 @@ if d==3:
 # Test DtN_loc accuracy:
 if d==3:
     # Here we'll test our DtN operators on a known function. First we define the known function and its
-    # first order derivatives:
+    # first order derivatives (this test is for Laplace only):
     def u_true(xx):
-        return torch.exp(xx[:,0]) * torch.sin(xx[:,1])
+        #return torch.exp(xx[:,0]) * torch.sin(xx[:,1])
+        return uu_dir_func_greens(3,xx,0)
     
     def du1_true(xx):
-        return torch.exp(xx[:,0]) * torch.sin(xx[:,1])
+        #return torch.exp(xx[:,0]) * torch.sin(xx[:,1])
+        return du_dir_func_greens(0,3,xx,0)
     
     def du2_true(xx):
-        return torch.exp(xx[:,0]) * torch.cos(xx[:,1])
+        #return torch.exp(xx[:,0]) * torch.cos(xx[:,1])
+        return du_dir_func_greens(1,3,xx,0)
     
     def du3_true(xx):
-        return torch.zeros((xx.shape[0]))
+        #return torch.zeros((xx.shape[0]))
+        return du_dir_func_greens(2,3,xx,0)
 
     size_face = dom.hps.p**2
     size_ext = 6 * size_face
@@ -324,10 +341,12 @@ if d==3:
 
     # Here we get our dirichlet data, reshape it, and then multiply with DtNs to get our Neumann data
     uu_dir_gauss = u_true(dom.hps.gauss_xx)
+    print(uu_dir_gauss.shape)
     uu_dir_gauss = torch.reshape(uu_dir_gauss, (DtN_loc.shape[0],-1))
+    print(uu_dir_gauss.shape)
     uu_dir_gauss = torch.unsqueeze(uu_dir_gauss, -1)
-    #print(DtN_loc.shape)
-    #print(uu_dir_gauss.shape)
+    print(DtN_loc.shape)
+    print(uu_dir_gauss.shape)
 
     uu_neumann_approx = torch.matmul(DtN_loc, uu_dir_gauss)
     uu_neumann_approx = torch.squeeze(uu_neumann_approx)
@@ -336,6 +355,8 @@ if d==3:
     # Next we fold our spatial inputs and compute our actual neumann data:
     xx_folded = torch.reshape(dom.hps.gauss_xx, (DtN_loc.shape[0],DtN_loc.shape[1], -1))
     #print(xx_folded.shape)
+    #np.set_printoptions(threshold=sys.maxsize)
+    #print(xx_folded[3,3*size_face:4*size_face,:].detach().numpy())
 
     uu_neumann = torch.zeros((xx_folded.shape[0], xx_folded.shape[1]))
     for i in range(xx_folded.shape[0]):
@@ -348,9 +369,12 @@ if d==3:
 
     print("Relative error of Neumann computation is")
     print(torch.linalg.norm(uu_neumann_approx - uu_neumann) / torch.linalg.norm(uu_neumann))
-
-    #print(uu_neumann.shape)
-    #print(uu_neumann_approx[0,:])
-    #print(uu_neumann[0,:])
-    #print(dom.hps.gauss_xx)
-"""
+    """
+    print(uu_neumann.shape)
+    print("DtN result:")
+    print(uu_neumann_approx[0,:])
+    print("True Neumann derivatives:")
+    print(uu_neumann[0,:])
+    #np.set_printoptions(threshold=sys.maxsize)
+    #print(dom.hps.gauss_xx.detach().numpy())
+    """

built_in_funcs.py
@@ -130,7 +130,7 @@ def uu_dir_func_greens(d,xx,kh,center=torch.tensor([-1.1,+1.,+1.2])):
         if d==2:
             uu_exact = (1/np.pi) * np.log(ddsq)
         else:
-            uu_exact = (1 / (4*np.pi)) * (1 / np.sqrt(ddsq))
+            uu_exact = 1 / (4 * np.pi * np.sqrt(ddsq))
     else:
         if d==2:
             dist_x = np.sqrt(ddsq)
@@ -140,6 +140,20 @@ def uu_dir_func_greens(d,xx,kh,center=torch.tensor([-1.1,+1.,+1.2])):
             dist_x = np.sqrt(ddsq)
             uu_exact = np.cos(kh * dist_x) / (4*np.pi * dist_x)
     return uu_exact.unsqueeze(-1)
+
+def du_dir_func_greens(deriv,d,xx,kh,center=torch.tensor([-1.1,+1.,+1.2])):
+    if d==2:
+        print("Error! This is only for d=3")
+    dd0 = xx[:,0] - center[0]
+    dd1 = xx[:,1] - center[1]
+    dd2 = xx[:,2] - center[2]
+    ddsq = np.multiply(dd0,dd0) + np.multiply(dd1,dd1) + np.multiply(dd2,dd2)
+    if kh==0:
+        dd = np.sqrt(ddsq)
+        du_exact = -(xx[:,deriv] - center[deriv]) / (4*np.pi * dd**3)
+        return du_exact
+    else:
+        print("Error! Not set for Helmholtz yet")
 
 def ff_body_pulse(xx,kh):
     

domain_driver.py
@@ -391,7 +391,7 @@ class Domain_Driver:
             sol_tot = torch.zeros(len(self.hps.I_unique),1)
             
             # Replace this with sol to do full system solve:
-            sol_tot[self.I_Ctot] = uu_dir_func(self.hps.xx_active[self.I_Ctot])
+            sol_tot[self.I_Ctot] = sol #uu_dir_func(self.hps.xx_active[self.I_Ctot])
             # Here we set the true exterior to the given data:
             sol_tot[self.I_Xtot] = uu_dir_func(self.hps.xx_active[self.I_Xtot])
 
@@ -408,12 +408,12 @@ class Domain_Driver:
             device = torch.device('cpu')
         tic = time()
 
-        GridX = self.hps.grid_xx.clone()
-        uu_true = torch.zeros((GridX.shape[0], GridX.shape[1],1))
-        for i in range(GridX.shape[0]):
-            uu_true[i] = uu_dir_func(GridX[i])
+        #GridX = self.hps.grid_xx.clone()
+        #uu_true = torch.zeros((GridX.shape[0], GridX.shape[1],1))
+        #for i in range(GridX.shape[0]):
+        #    uu_true[i] = uu_dir_func(GridX[i])
         
-        sol_tot,resloc_hps = self.hps.solve(device,sol_tot,ff_body_func=ff_body_func,uu_true=uu_true)
+        sol_tot,resloc_hps = self.hps.solve(device,sol_tot,ff_body_func=ff_body_func,uu_true=None)
         toc_solve += time() - tic
         sol_tot = sol_tot.cpu()
 

hps_parallel_leaf_ops.py
@@ -117,11 +117,10 @@ def Aloc_acc(p, d, nboxes, xx_flat, Aloc, func, D, c=1.):
     
 def form_DtNs(p,d,xxloc,Nx,Jx,Jc,Jxreo,Jxun,Ds,Intmap,Intmap_rev,Intmap_unq,pdo,
           box_start,box_end,device,mode,data,ff_body_func,uu_true):
+    args = p,xxloc,Ds,pdo,box_start,box_end
     if (d == 2):
-        args = p,xxloc,Ds,pdo,box_start,box_end
         Aloc = get_Aloc_2d(*args,device)
     else:
-        args = p,xxloc,Ds,pdo,box_start,box_end
         Aloc = get_Aloc_3d(*args,device)
     Acc = Aloc[:,Jc,:][:,:,Jc]
     nrhs = data.shape[-1]
@@ -136,12 +135,13 @@ def form_DtNs(p,d,xxloc,Nx,Jx,Jc,Jxreo,Jxun,Ds,Intmap,Intmap_rev,Intmap_unq,pdo,
             
             DtN     = Nx[...,Jtot].unsqueeze(0) @ S_full
         else:
-            S_tmp   = -torch.linalg.solve(Acc,Aloc[:,Jc][...,Jxun]) # Should this be Jxun? Seems different from solve
-            Irep    = torch.eye(Jxreo.shape[0],device=device).unsqueeze(0).repeat(box_end-box_start,1,1)
-            S_full  = torch.concat((S_tmp @ Intmap_unq.unsqueeze(0),Irep),dim=1)
+            S_tmp   = -torch.linalg.solve(Acc,Aloc[:,Jc][...,Jxun]) # Should append Identity here and not repeat Intmap
+            Intmap_repeat = Intmap_unq.unsqueeze(0).repeat(box_end-box_start,1,1)
+            S_full        = torch.concat((S_tmp @ Intmap_unq.unsqueeze(0),Intmap_repeat),dim=1)
             
-            Jtot    = torch.hstack((Jc,Jxreo)) # Might be Jx instead of Jxreo
+            Jtot    = torch.hstack((Jc,Jxun))
             DtN     = Nx[:,Jtot].unsqueeze(0) @ S_full
+            print(DtN.shape)
             if d==3:
                 DtN = Intmap_rev.unsqueeze(0) @ DtN
         return DtN

some_plots.py
@@ -16,9 +16,12 @@ inversionTime = [0.03, 0.36, 1.83, 5.08, 13.00, 51.48, 168.02, 434.81]
 
 solveTime = [0.11, 0.34, 0.84, 1.69, 3.04, 7.19, 14.55, 31.59]
 
-leafSolveError = [5.06e-04, 9.12e-05, 1.49e-05, 6.35e-06, 5.95e-06, 9.16e-07, 6.17e-07, 1.57e-07]
+#leafSolveError = [5.06e-04, 9.12e-05, 1.49e-05, 6.35e-06, 5.95e-06, 9.16e-07, 6.17e-07, 1.57e-07]
+# Updated leafSolveError away from singularity point:
+leafSolveError = [1.10e-13, 3.70e-14, 1.31e-14, 1.45e-14, 3.44e-14, 4.18e-15, 7.56e-15, 2.67e-15]
 
 interpolationError = [0.0154, 0.0160, 0.0005, 5.3633e-05, 1.4921e-05, 5.8685e-07, 2.2868e-07, 3.9676e-08]
+interpolationError = [1.1760e-14, 2.3474e-15, 2.6497e-15, 1.1555e-15, 3.4974e-15]
 """
 plt.plot(nBoxes, assemblyTime)
 plt.title("Assembly Times, p=12")
@@ -70,23 +73,34 @@ plt.show()
 #nBoxes = (10*nBoxes) ** 3
 
 plt.semilogy(nBoxes, leafSolveError)
-plt.semilogy(nBoxes, interpolationError)
+#plt.semilogy(nBoxes, interpolationError)
 plt.title("Accuracy for Laplace equation, p=12")
 plt.xlabel("Degrees of freedom (nodes per box X number of boxes)")
 plt.ylabel("Relative error")
-plt.legend(["Leaf solve", "Box interpolation"])
+#plt.legend(["Leaf solve", "Box interpolation"])
 plt.show()
 
 
 # P refinement tests with Helmholtz, 10 ppw
+# Residual errors: ||A_loc u_true - f||
+
 N12 = np.array([20, 30, 40, 50, 60, 70, 80]) ** 3
-trueError12 = [9.53e-04, 2.00e-04, 4.28e-05, 4.64e-05, 1.63e-04, 1.47e-04, 1.44e-04]
-# Residual errors: ||A_loc u - f||
-resError12 = [8.90e-11, 3.96e-10, 1.09e-09, 2.66e-09, 4.64e-09, 9.35e-09, 1.28e-08]
+# New, better results are bottom:
+#trueError12 = [9.53e-04, 2.00e-04, 4.28e-05, 4.64e-05, 1.63e-04, 1.47e-04, 1.44e-04]
+#resError12 = [8.90e-11, 3.96e-10, 1.09e-09, 2.66e-09, 4.64e-09, 9.35e-09, 1.28e-08]
+trueError12 = [7.77e-07, 1.45e-06, 6.67e-06, 2.86e-05, 1.38e-04, 1.28e-04, 1.28e-04]
+resError12 = [4.23e-11, 1.81e-10, 4.72e-10, 1.14e-09, 1.99e-09, 3.96e-09, 5.45e-09]
+
 
 N18 = np.array([32, 48, 64, 80]) ** 3
-trueError18 = [8.21e-04, 2.50e-04, 5.49e-04, 2.78e-03]
-resError18 = [2.05e-09, 1.18e-08, 4.14e-08, 5.50e-08]
+#trueError18 = [8.21e-04, 2.50e-04, 5.49e-04, 2.78e-03]
+#resError18 = [2.05e-09, 1.18e-08, 4.14e-08, 5.50e-08]
+trueError18 = [1.25e-05, 3.99e-05, 4.44e-04, 2.37e-03]
+resError18 = [9.01e-10, 5.14e-09, 1.84e-08, 2.32e-08]
+
+N22 = np.array([40, 60, 80]) ** 3
+trueError22 = [4.04e-04, 2.73e-03, 2.13e-02]
+resError22 = [5.61e-09, 2.27e-08, 6.89e-08]
 
 plt.semilogy(N12, trueError12, 'b-')
 plt.semilogy(N18, trueError18, 'r-')