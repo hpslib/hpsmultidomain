28f849bc32f7aea59a6412c22e9484557f4f3178
Joseph <josek97@utexas.edu>
2024-05-21 17:37:57 -0500

    Modified testing file

--- run_models.py
+++ run_models.py
@@ -1,18 +1,18 @@
-import numpy as np
+#import numpy as np
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from test_3d import run_test_via_argparse
 
 # First we'll run a suite of Poisson problems for different n and p:
-p_list = [4, 6]#, 8, 10, 12, 14, 18, 22, 30]
+p_list = [10] #[6, 8, 10, 12, 14, 18, 22, 30]
 
 domain = "square"
 pde = "poisson"
 bc = "log_dist"
 
 for p in p_list:
-    n_list = list(range(2*(p-2), 100, p-2))
+    n_list = list(range(2*(p-2), min(100,11*(p-2)), p-2))
 
     for n in n_list:
         print(str(p) + ", " + str(n) + " done")
