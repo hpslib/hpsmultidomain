1cd73795ba6f1081979d3853281a8a4cf896929e
Joseph <josek97@utexas.edu>
2024-05-22 20:54:43 -0500

    Typos

--- run_models.py
+++ run_models.py
@@ -5,7 +5,8 @@
 from test_3d import run_test_via_argparse
 
 # First we'll run a suite of Poisson problems for different n and p:
-p_list = [8, 10, 12, 14, 18, 22]
+#p_list = [10, 12, 14, 18, 22]
+p_list = [24]
 
 domain = "square"
 pde = "bfield_constant"
@@ -19,4 +20,4 @@ for p in p_list:
     for n in n_list:
         print(str(p) + ", " + str(n) + " done")
         test_file = "test_helmholtz/test_results_p_" + str(p) + "_n_" + str(n) + ".pkl"
-        run_test_via_argparse(domain, pde, bc, n, p, ppw=ppw, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, ppw=1, components=False, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
+        run_test_via_argparse(domain, pde, bc, n, p, ppw=ppw, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, components=False, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
