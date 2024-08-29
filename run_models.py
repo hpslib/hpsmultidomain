1bab4295fc636b757f2721bd64c257b50797fd7c
jlk9 <josek97@utexas.edu>
2024-08-28 19:54:53 -0500

    Testing curved domain

--- run_models.py
+++ run_models.py
@@ -5,11 +5,11 @@
 from test_3d import run_test_via_argparse
 
 # First we'll run a suite of Poisson problems for different n and p:
-p_list = [10, 12, 14, 16, 18, 20, 22, 24, 26]
+p_list = [10, 12, 14, 16, 18, 20, 22]#, 24, 26]
 #p_list = [30]
 
-domain = "square"
-pde = "bfield_variable"
+domain = "curved"
+pde = "bfield_constant"
 bc = "free_space"
 ppw = 10
 
@@ -19,5 +19,5 @@ for p in p_list:
     
     for n in n_list:
         print(str(p) + ", " + str(n) + " done")
-        test_file = "test_variable/test_results_p_" + str(p) + "_n_" + str(n) + ".pkl"
+        test_file = "test_curve/test_results_p_" + str(p) + "_n_" + str(n) + ".pkl"
         run_test_via_argparse(domain, pde, bc, n, p, ppw=ppw, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, components=False, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
