9a6ab661069b74750c6901ba1313af44295247d5
Joseph <josek97@utexas.edu>
2025-03-28 17:33:10 -0500

    minor edits

--- test/run_models.py
+++ test/run_models.py
@@ -10,21 +10,22 @@ from test_3d import run_test_via_argparse
 #p_list = [10, 12, 14, 16, 18]
 #p_list = [22]
 
-#p_list = [10,12,14,16,18,20,22]
+p_list = [10,12,14,16,18,20,22]
 #p_list = [18,20,22] 
 #p_list = [20,22]
-p_list = [12, 14]
+#p_list = [6, 8, 10, 12, 14]
 
 #p_list = [9,11,13,15,17,19,21]
 
 domain = "square"
-pde = "poisson"
-bc = "log_dist"
-ppw = None
+pde = "bfield_constant"
+bc = "free_space"
+ppw = 10
 kh = None
 delta_t = None
+store_sol = False
 
-output_path = "output/poisson_0327"
+output_path = "output/helmholtz_10ppw_0327"
 
 for p in p_list:
     n_list = list(range(2*(p-2), 210, p-2))
@@ -35,4 +36,4 @@ for p in p_list:
     for n in n_list:
         print("\nRunning " + str(p) + ", " + str(n) + "\n")
         test_file = output_path + "/test_results_p_" + str(p) + "_n_" + str(n) + ".pkl"
-        run_test_via_argparse(domain, pde, bc, n, p, ppw=ppw, kh=kh, delta_t=delta_t, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, components=False, store_sol=True, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
+        run_test_via_argparse(domain, pde, bc, n, p, ppw=ppw, kh=kh, delta_t=delta_t, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, components=False, store_sol=store_sol, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
