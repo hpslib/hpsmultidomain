23e25c38f626c290ab49532151eb6677bc8ad7ac
Anna Yesypenko <annayesypenko@gmail.com>
2025-08-19 09:11:23 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='hps',
     version='1.0',
-    packages=['src'],
+    packages=['hps'],
     license="MIT",
     author="Joseph Kump, Anna Yesypenko",
     url='https://github.com/annayesy/hps-multidomain-disc',
