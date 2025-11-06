1231436bb78e8ed3a471c539606ecb71ef2e0e2f
jlk9 <josek97@utexas.edu>
2025-11-06 10:01:53 -0600

    Updates to analytic reformulation of twisted torus

--- hps/pdo.py
+++ hps/pdo.py
@@ -257,6 +257,43 @@ def G_components(parameter_map, y_1, y_2, y_3):
 
     return (G11, G22, G33, G12, G13, G23), (C1, C2, C3, det_J)
 
+# Derivatives of C at all entries, 3x3x3 tensor
+def dC_columns(yd1, yd2, yd3):
+    dC = [[None for _ in range(3)] for _ in range(3)]
+    for i in range(3):
+        y1g = y_col_di()
+        y2g = y_col_di()
+        y3g = y_col_di()
+        # Will need to change this to work for lists / tuples
+        dC[1][i] = cross_func(y2g, yd3) + cross_func(yd2, y3g)
+        dC[2][i] = cross_func(y3g, yd1) + cross_func(yd3, y1g)
+        dC[3][i] = cross_func(y1g, yd2) + cross_func(yd1, y2g)
+    return dC
+
+
+# Produces the derivatives for G, the matrix J^-1 J^-T
+def dG_di(parameter_map, G, C, detJ):
+    dC = dC_columns()
+    grad_detJ = d_detJ
+    # G is a matrix, so dG is a tensor:
+    dG = [[[None for _ in range(3)] for _ in range(3)] for _ in range(3)]
+
+## Constructing the first-order coefficients
+def first_order_coeffs(parameter_map, G, C, detJ):
+    dG = dG_di()
+    d_detJ = d_detJ()
+    def grad_logJ(xx):
+        return torch.div(d_detJ(xx), detJ(xx))
+
+    (G11, G22, G33, G12, G13, G23) = G
+    Gmat = [[G11, G12, G13],
+            [G12, G22, G23],
+            [G13, G23, G33]]
+
+    # Constructing the actual first-order derivatives:
+    b = []
+    for i in range(3):
+        b.append(None)
 
 
 #####################################################################################
@@ -355,21 +392,30 @@ def pdo_param_3d(kh, bfield, z1, z2, z3, y1, y2, y3, y1_d1=None, y1_d2=None, y1_
         YY[:,2] = y3(xx)
         return YY
 
+    yi_djdk = [[[y1_d1d1, y1_d1d2, ]]]
+
+    (G11, G22, G33, G12, G13, G23), (C1, C2, C3, det_J) = G_components(parameter_map, (y1_d1, y1_d2, y1_d3), (y2_d1, y2_d2, y2_d3), (y3_d1, y3_d2, y3_d3))
+
+    dC = dC_columns()
+
     # What we need to change for into divergence-form
-    c11 = cii_func(parameter_map,y1_d1,y1_d2,y1_d3)
-    c22 = cii_func(parameter_map,y2_d1,y2_d2,y2_d3)
-    c33 = cii_func(parameter_map,y3_d1,y3_d2,y3_d3)
+    #c11 = cii_func(parameter_map,y1_d1,y1_d2,y1_d3)
+    #c22 = cii_func(parameter_map,y2_d1,y2_d2,y2_d3)
+    #c33 = cii_func(parameter_map,y3_d1,y3_d2,y3_d3)
     c1  = ci_func (parameter_map,y1_d1d1,y1_d2d2,y1_d3d3)
     c2  = ci_func (parameter_map,y2_d1d1,y2_d2d2,y2_d3d3)
     c3  = ci_func (parameter_map,y3_d1d1,y3_d2d2,y3_d3d3)
-    c12 = cij_func(parameter_map,y1_d1,y2_d1,y1_d2,y2_d2,y1_d3,y2_d3)
-    c13 = cij_func(parameter_map,y1_d1,y3_d1,y1_d2,y3_d2,y1_d3,y3_d3)
-    c23 = cij_func(parameter_map,y2_d1,y3_d1,y2_d2,y3_d2,y2_d3,y3_d3)
+    #c12 = cij_func(parameter_map,y1_d1,y2_d1,y1_d2,y2_d2,y1_d3,y2_d3)
+    #c13 = cij_func(parameter_map,y1_d1,y3_d1,y1_d2,y3_d2,y1_d3,y3_d3)
+    #c23 = cij_func(parameter_map,y2_d1,y3_d1,y2_d2,y3_d2,y2_d3,y3_d3)
     
     def c(xx):
         return bfield(parameter_map(xx),kh)
     
-    pdo = PDO_3d(c11=c11,c22=c22,c33=c33,c1=c1,c2=c2,c3=c3,c12=c12,c13=c13,c23=c23,c=c)
+    #pdo = PDO_3d(c11=c11,c22=c22,c33=c33,c1=c1,c2=c2,c3=c3,c12=c12,c13=c13,c23=c23,c=c)
+    pdo = PDO_3d(c11=G11, c22=G22, c33=G33, c1=c1, c2=c2, c3=c3,
+                 c12 = lambda xx: 2*G12(xx), c13 = lambda xx: 2*G13(xx), c23 = lambda xx: 2*G23(xx),
+                 c=c)
     return pdo, parameter_map, inv_parameter_map
 
 
