b9ded6baeedc2d52fe1f6df87769993a6fb04169
jlk9 <josek97@utexas.edu>
2026-02-16 16:02:58 -0600

    Fallback

--- hpsmultidomain/hps_multidomain_disc.py
+++ hpsmultidomain/hps_multidomain_disc.py
@@ -52,10 +52,10 @@ class HPS_Multidomain:
         self.periodic_bc = periodic_bc
 
         # For interpolation:
-        self.interpolate = True
+        self.interpolate = False
         self.q = self.p - 2
 
-        
+        """
         if d==2:
             if (pdo.c12 is None):
                 self.interpolate = False
@@ -64,7 +64,7 @@ class HPS_Multidomain:
             if (pdo.c12 is None) and (pdo.c13 is None) and (pdo.c23 is None):
                 self.interpolate = False
                 self.q = self.p - 2
-        
+        """
 
         n = (self.domain[:,1] - self.domain[:,0]) / (2*self.a)
         # if n is a numpy array, make it a tensor
