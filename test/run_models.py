4c47f71f00b337ef1ad3126ae2fff1eeeceb43a0
jlk9 <josek97@utexas.edu>
2026-05-12 01:57:34 -0500

    Run test fix

--- test/run_models.py
+++ test/run_models.py
@@ -2,7 +2,7 @@
 
 #import matplotlib.pyplot as plt
 
-from test_3d import run_test_via_argparse
+from test_3d import run_test_case
 
 # Can set different parameters for a longer test here:
 # ranges of p, domain shape, pde and bc of choice,
@@ -24,4 +24,4 @@ for p in p_list:
     for n in n_list:
         print("\nRunning " + str(p) + ", " + str(n) + "\n")
         test_file = output_path + "/test_results_p_" + str(p) + "_n_" + str(n) + ".pkl"
-        run_test_via_argparse(domain, pde, bc, n, p, ppw=ppw, kh=kh, delta_t=delta_t, num_timesteps=10, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, components=False, store_sol=True, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
+        run_test_case(domain=domain, pde=pde, bc=bc, n=n, p=p, ppw=ppw, kh=kh, delta_t=delta_t, num_timesteps=None, box_xlim=1.0, box_ylim=1.0, periodic_bc=False, components=False, store_sol=True, solver="mumps", assembly_type="reduced_gpu", pickle_loc=test_file)
